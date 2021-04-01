a763a18819e6d5f93955f05afdbb3efbe37a2cd5
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-04-01 22:41:05 +0900

    プロフィール画面を追加

--- pages/index.js
+++ pages/index.js
@@ -18,31 +18,34 @@ export default function Home() {
 				<div className={styles.mainView}>
 					<h1 className={styles.title}>Port Folio Site</h1>
 				</div>
-				<div className={styles.profile}>
-					<div>
-						<h3 className={styles.profileName}>池内隆人</h3>
-						<p className={styles.profileJob}>~ Web Engineer ~</p>
-						<div className={styles.profileText}>
-							<p>
-								趣味：お酒、古着、野球観戦、ドライブ
-								<br />
-								現在広島県に在住の21歳です。
-								<br />
-								小さい頃からものづくりが好きで
-								<br />
-								自分のつくったサービスを通してたくさんの人を
-								<br />
-								幸せにしたいと思いエンジニアを目指しています。
-							</p>
+				<section className={styles.profile}>
+					<h2 className={styles.profileTitle}>Profile</h2>
+					<article className={styles.profileContent}>
+						<div className={styles.profileContainer}>
+							<h3 className={styles.profileName}>池内隆人</h3>
+							<p className={styles.profileJob}>~ Web Engineer ~</p>
+							<div className={styles.profileText}>
+								<p>趣味：お酒、古着、野球観戦、ドライブ</p>
+								<p>
+									<br />
+									現在広島県に在住の21歳です。
+									<br />
+									小さい頃からものづくりが好きで
+									<br />
+									自分のつくったサービスを通してたくさんの人を
+									<br />
+									幸せにしたいと思いエンジニアを目指しています。
+								</p>
+							</div>
 						</div>
-					</div>
-					<Image
-						src="/images/ryuto.jpg"
-						alt="Picture of the author"
-						width={300}
-						height={250}
-					/>
-				</div>
+						<Image
+							src="/images/ryuto.png"
+							alt="Picture of the author"
+							width={240}
+							height={240}
+						/>
+					</article>
+				</section>
 			</main>
 
 			<footer className={styles.footer}>©️ 2021 ryutoikeuchi</footer>
