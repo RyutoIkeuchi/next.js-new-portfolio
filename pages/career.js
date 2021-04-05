2b99fb04e78323830407693084c14e7947b8bcbe
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-04-05 21:29:29 +0900

    careerページを編集

--- pages/career.js
+++ pages/career.js
@@ -16,11 +16,13 @@ export default function Career() {
 			<main className={styles.main}>
 				<SectionTitle
 					title="Career"
-					text="岡山(1999~2018) ~ 広島(2018~2021) ~ 東京(2021~)？？"
+					text="岡山(1999~2018)~広島(2018~2021)~東京(2021)~？？"
+					text2="将来の夢は多くの人に影響を与えるサービスをつくること"
+					text3="好きな漫画はキングダム・デスノート・テラフォーマーズ"
 				/>
 				<section className={styles.careerContainer}>
 					<div className={styles.careerBorder}></div>
-					<CareerContent/>
+					<CareerContent />
 				</section>
 			</main>
 			<Footer />
