95eb01ef559943d25ab8622a18cca981e9082fa0
Ikeuchi Ryuto <ryu.i7133@gmail.com>
2021-04-07 14:39:18 +0900

    worksページを編集

--- components/Works/WorksGrid.js
+++ components/Works/WorksGrid.js
@@ -1,31 +1,17 @@
 import styles from '../../styles/Home.module.css';
-import {WorksWrapper} from './WorksWrapper'
+import { WorksWrapper } from './WorksWrapper'
+import {WorksPeace} from './WorksPeace'
 
 export const WorksGrid = () => {
   return (
 		<section className={styles.gridStyle}>
-			<WorksWrapper
-				url="https://ryutoikeuchi.github.io/isara-/"
-				language="HTML&CSS,jQuery"
-				title="isara模写"
-			/>
-			<WorksWrapper
-				url="https://ryutoikeuchi.github.io/vuejs.todoapp/"
-				language="Vue.js"
-				title="Todoリスト"
-			/>
-			<WorksWrapper
-				url="https://gifted-cori-a4da38.netlify.app"
-				language="React.js"
-				title="写真検索アプリ"
-			/>
-			<WorksWrapper
-				url="https://next-js-new-portfolio.vercel.app/"
-				language="Next.js"
-				title="自己紹介サイト"
-			/>
-			<WorksWrapper url="#" language="Comming" title="soon" />
-			<WorksWrapper url="#" language="Comming" title="soon" />
+			{WorksPeace.map((peace) => (
+				<WorksWrapper
+					url={peace.url}
+					language={peace.language}
+					title={peace.title}
+				/>
+			))}
 		</section>
 	);
 }
---
+++ components/Works/WorksPeace.js
@@ -0,0 +1,32 @@
+export const WorksPeace = [
+	{
+		url: 'https://ryutoikeuchi.github.io/isara-/',
+		language: 'HTML&CSS,jQuery',
+		title: 'isara模写',
+	},
+	{
+		url: 'https://ryutoikeuchi.github.io/vuejs.todoapp/',
+		language: 'Vue.js',
+		title: 'Todoリスト',
+	},
+	{
+		url: 'https://gifted-cori-a4da38.netlify.app',
+		language: 'React.js',
+		title: '写真検索アプリ',
+	},
+	{
+		url: 'https://next-js-new-portfolio.vercel.app/',
+		language: 'Next.js',
+		title: '自己紹介サイト',
+	},
+	{
+		url: '#',
+		language: 'Comming',
+		title: 'soon',
+	},
+	{
+		url: '#',
+		language: 'Comming',
+		title: 'soon',
+	},
+];
